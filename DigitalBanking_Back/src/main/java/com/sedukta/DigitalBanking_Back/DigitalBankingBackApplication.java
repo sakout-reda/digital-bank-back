b7467fc89fafd524349749c05ab167351687c4ba
sakout-reda <sakout-reda@hotmail.fr>
2022-08-13 13:26:31 +0100

    Initial Commit

---
+++ DigitalBanking_Back/src/main/java/com/sedukta/DigitalBanking_Back/DigitalBankingBackApplication.java
@@ -0,0 +1,13 @@
+package com.sedukta.DigitalBanking_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingBackApplication.class, args);
+	}
+
+}
---
+++ DigitalBanking_Back/src/test/java/com/sedukta/DigitalBanking_Back/DigitalBankingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedukta.DigitalBanking_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
